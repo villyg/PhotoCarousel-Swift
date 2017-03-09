7db4ab07468d0f150d1f1ab382033981cb9a3f51
Villy G <villyg@icloud.com>
2017-03-08 20:53:30 -0500

    Added collection view

---
+++ PhotoCarousel/ImageCell.swift
@@ -0,0 +1,42 @@
+//
+//  ImageCell.swift
+//  PhotoCarousel
+//
+//  Created by Veliyan Georgiev on 3/8/17.
+//  Copyright © 2017 VillyG. All rights reserved.
+//
+
+import UIKit
+
+class ImageCell: UICollectionViewCell {
+    
+    static let reuseIdentifer: String = String(describing: type(of: self))
+    
+    var imageView: UIImageView!
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        imageView = UIImageView()
+        contentView.addSubview(imageView)
+        backgroundColor = UIColor.yellow
+        imageView.contentMode = .scaleAspectFit
+        imageView.clipsToBounds = true
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.widthAnchor.constraint(equalTo: contentView.widthAnchor, multiplier: 1.0).isActive = true
+        imageView.heightAnchor.constraint(equalTo: contentView.heightAnchor, multiplier: 1.0).isActive = true
+        
+    }
+    
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    
+//    override func preferredLayoutAttributesFitting(_ layoutAttributes: UICollectionViewLayoutAttributes) -> UICollectionViewLayoutAttributes {
+//        let attributes = super.preferredLayoutAttributesFitting(layoutAttributes)
+//        return attributes
+//    }
+    
+}
--- PhotoCarousel/ViewController.swift
+++ PhotoCarousel/ViewController.swift
@@ -11,18 +11,39 @@ import UIKit
 class MainController: UIViewController {
     
     
-    let image1: UIImage = UIImage.create(fromColor: UIColor.blue)
-    let image2: UIImage = UIImage.create(fromColor: UIColor.red)
-    let image3: UIImage = UIImage.create(fromColor: UIColor.yellow)
-    
-    var carousel: UIView = {
+    var images: [UIImage] = {
+
+        let image1: UIImage = UIImage(named: "image1")!
+        let image2: UIImage = UIImage(named: "image2")!
+        let image3: UIImage = UIImage(named: "image3")!
+        let image4: UIImage = UIImage(named: "image4")!
         
-        let result = UIView()
-        result.translatesAutoresizingMaskIntoConstraints = false
-        result.backgroundColor = UIColor.purple
+        var result: [UIImage] = []
+        result.append(image1)
+        result.append(image2)
+        result.append(image3)
+        result.append(image4)
+        
         return result
         
     }()
+    
+    
+    var collectionView: UICollectionView = {
+        
+        let frame = CGRect(x: 0, y: 0, width: 0, height: 0)
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .horizontal
+        
+        let result = UICollectionView(frame: frame, collectionViewLayout: layout)
+        result.backgroundColor = UIColor.darkGray
+        result.isPagingEnabled = true
+        result.translatesAutoresizingMaskIntoConstraints = false
+        
+        return result
+        
+    }()
+    
     
     var commonConstraints: [NSLayoutConstraint] = []
     var landscapeConstraints: [NSLayoutConstraint] = []
@@ -31,31 +52,36 @@ class MainController: UIViewController {
     var firstTimeSetup: Bool = false
     
     
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        self.view.addSubview(carousel)
+        self.collectionView.register(ImageCell.self, forCellWithReuseIdentifier: ImageCell.reuseIdentifer)
+        self.collectionView.delegate = self
+        self.collectionView.dataSource = self
+        self.automaticallyAdjustsScrollViewInsets = false
+        self.view.addSubview(collectionView)
         
     }
     
-    
-    override func viewWillLayoutSubviews() {
-        super.viewWillLayoutSubviews()
-    }
     
     
     override func traitCollectionDidChange(_ previousTraitCollection: UITraitCollection?) {
         super.traitCollectionDidChange(previousTraitCollection)
         
+        print(#function)
+        
+        
         if !firstTimeSetup {
             
-            commonConstraints.append(NSLayoutConstraint(item: carousel, attribute: .leading, relatedBy: .equal, toItem: view, attribute: .leading, multiplier: 1.0, constant: 0))
-            commonConstraints.append(NSLayoutConstraint(item: carousel, attribute: .top, relatedBy: .equal, toItem: self.topLayoutGuide, attribute: .bottom, multiplier: 1.0, constant: 0))
-            commonConstraints.append(NSLayoutConstraint(item: carousel, attribute: .trailing, relatedBy: .equal, toItem: view, attribute: .trailing, multiplier: 1.0, constant: 0))
+            commonConstraints.append(NSLayoutConstraint(item: collectionView, attribute: .leading, relatedBy: .equal, toItem: view, attribute: .leading, multiplier: 1.0, constant: 0))
+            commonConstraints.append(NSLayoutConstraint(item: collectionView, attribute: .top, relatedBy: .equal, toItem: self.topLayoutGuide, attribute: .bottom, multiplier: 1.0, constant: 0))
+            commonConstraints.append(NSLayoutConstraint(item: collectionView, attribute: .trailing, relatedBy: .equal, toItem: view, attribute: .trailing, multiplier: 1.0, constant: 0))
             
-            portraitConstraints.append(NSLayoutConstraint(item: carousel, attribute: .height, relatedBy: .equal, toItem: nil, attribute: .notAnAttribute, multiplier: 1.0, constant: self.view.bounds.height / 3))
-            
-            landscapeConstraints.append(NSLayoutConstraint(item: carousel, attribute: .height, relatedBy: .equal, toItem: nil, attribute: .notAnAttribute, multiplier: 1.0, constant: self.view.bounds.height))
+            portraitConstraints.append(NSLayoutConstraint(item: collectionView, attribute: .height, relatedBy: .equal, toItem: self.view, attribute: .height, multiplier: 0.33, constant: 0))
+
+            landscapeConstraints.append(NSLayoutConstraint(item: collectionView, attribute: .height, relatedBy: .equal, toItem: self.view, attribute: .height, multiplier: 1.0, constant: 0))
+
             
             
             NSLayoutConstraint.activate(commonConstraints)
@@ -64,7 +90,10 @@ class MainController: UIViewController {
         
         }
         
+        
+        
         if traitCollection.verticalSizeClass != previousTraitCollection?.verticalSizeClass {
+        
             
             NSLayoutConstraint.deactivate(landscapeConstraints)
             NSLayoutConstraint.deactivate(portraitConstraints)
@@ -75,6 +104,9 @@ class MainController: UIViewController {
             } else {
                 NSLayoutConstraint.activate(landscapeConstraints)
             }
+            
+            self.collectionView.collectionViewLayout.invalidateLayout()
+
         }
         
     }
@@ -85,43 +117,105 @@ class MainController: UIViewController {
     
     override func willTransition(to newCollection: UITraitCollection, with coordinator: UIViewControllerTransitionCoordinator) {
         super.willTransition(to: newCollection, with: coordinator)
-        
+
+        print(#function)
+    
         coordinator.animate(alongsideTransition: { (context) in
-            
+        
+            print("will transition to alongside")
+
             if newCollection.verticalSizeClass == .regular {
                 print("regular vertical")
-                //                self.view.backgroundColor = UIColor.red
             } else {
                 print("compact vertical")
-                //                self.view.backgroundColor = UIColor.green
             }
             
+            
         }) { (context) in
-            //            self.view.backgroundColor = UIColor.blue
+            
+            print("willTransitionTo: completion")
+
         }
         
     }
     
-    //
-    //
-    //
-    //
-    //    override func viewWillTransition(to size: CGSize, with coordinator: UIViewControllerTransitionCoordinator) {
-    //        super.viewWillTransition(to: size, with: coordinator)
-    //
-    //
-    //        coordinator.animate(alongsideTransition: { (context: UIViewControllerTransitionCoordinatorContext) in
-    //
-    //
-    ////            self.tableView.endUpdates()
-    //
-    //        }) { (context: UIViewControllerTransitionCoordinatorContext) in
-    //            
-    //            
-    //        }
-    //        
-    //    }
     
+    
+    
+    
+//        override func viewWillTransition(to size: CGSize, with coordinator: UIViewControllerTransitionCoordinator) {
+//            super.viewWillTransition(to: size, with: coordinator)
+//    
+//            print(#function)
+//            
+//            coordinator.animate(alongsideTransition: { (context: UIViewControllerTransitionCoordinatorContext) in
+//    
+//    
+//            }) { (context: UIViewControllerTransitionCoordinatorContext) in
+//                
+//
+//                
+//            }
+//            
+//        }
     
 }
 
+
+
+
+
+
+extension MainController: UICollectionViewDataSource {
+
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+    
+        return self.images.count
+    }
+    
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+    
+
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: ImageCell.reuseIdentifer, for: indexPath) as! ImageCell
+        
+        let image: UIImage = images[indexPath.row]
+        
+        cell.imageView.image = image
+        
+        return cell
+        
+    }
+
+}
+
+
+
+
+extension MainController: UICollectionViewDelegateFlowLayout {
+
+
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+
+        return collectionView.frame.size
+    
+    }
+    
+    
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        
+        return 0.0
+    
+    }
+    
+    
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+       
+        return 0.0
+    
+    }
+    
+}
